f0cc534222ccd794ceebcb08037f71ce27c20317
nitroo420 <82112420+nitroo420@users.noreply.github.com>
2021-04-07 23:47:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let nidzaMajmun = 25;
+
+console.log(nidzaMajmun);
+
+switch(nidzaMajmun) {
+  case 5: 
+    console.log('Nidza je majmun puta 5');
+    break;
+  case 10:
+    console.log('Nidza je majmun puta 10');
+    break;
+  case 15:
+   console.log('Nidza je majmun puta 15');
+   break;
+  case 20:
+    console.log('Nidza je majmun puta 20');
+    break;
+  case 25:
+    console.log('Nidza je majmun puta 25');
+    break;
+  default:
+    console.log('Nidza je orangutan');
+    break;
+    
+}
